8dfec69143a017b7c34e4e152653c2b7dab87ab0
MiceonVenus0306 <adorwamagst@outlook.com>
2025-07-30 18:24:26 +0800

    Initial Commit

---
+++ MemoPair/MemoPairApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoPairApp.swift
+//  MemoPair
+//
+//  Created by 李 on 2025/7/30.
+//
+
+import SwiftUI
+
+@main
+struct MemoPairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
